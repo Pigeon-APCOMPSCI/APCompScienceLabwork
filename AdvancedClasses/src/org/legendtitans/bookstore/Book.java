d83218bca2ea5e024894e45ee6ea7ef1a898d3f6
Josh <snickers07@comcast.net>
2021-02-08 13:47:30 -0700

    Added in both classes required as well as skeletons of functions needed

---
+++ AdvancedClasses/src/org/legendtitans/bookstore/Book.java
@@ -0,0 +1,27 @@
+package org.legendtitans.bookstore;
+
+public class Book {
+
+	private String name;
+	private String isbn;
+	
+	public Book(String n, String isbn) {
+		name = n;
+		this.isbn = isbn;
+	}
+	
+	public String getName() {		
+		return name;
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+}
---
+++ AdvancedClasses/src/org/legendtitans/bookstore/BookStoreRunner.java
@@ -0,0 +1,26 @@
+package org.legendtitans.bookstore;
+
+import java.util.ArrayList;
+
+public class BookStoreRunner {
+
+	public static void main(String[] args) {
+		ArrayList<Book> availableBooks = new ArrayList<Book>();
+		ArrayList<Book> yourBooks = new ArrayList<Book>();
+
+		listAvailableBooks();
+		
+
+	}
+	
+	public static void listAvailableBooks() {
+		
+	}
+	public static void determineBookSelection() {
+		
+	}
+	public static void addToYourBooks() {
+		
+	}
+
+}
